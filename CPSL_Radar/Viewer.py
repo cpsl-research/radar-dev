8fe787118f9c24b621f1c887e586a85555624c82
David Hunt <david.hunt@duke.edu>
2023-08-22 21:09:49 -0400

    Implemented ability to generate a movie of results

--- CPSL_Radar/Viewer.py
+++ CPSL_Radar/Viewer.py
@@ -101,18 +101,25 @@ class Viewer:
 
         #save each frame
         for i in tqdm(range(self.dataset_generator.num_samples)):
+            
+            #clear the axes
+            for ax in axs.flat:
+                ax.cla()
 
+            #plot the result
             self.view_result(
                 sample_idx=i,
                 axs=axs,
                 show=False
             )
 
+            #save the figure
             buf = io.BytesIO()
             fig.savefig(buf,format='png',dpi=300)
             buf.seek(0)
             image_frames.append(imageio.imread(buf))
-        
+
+        #save the end result to a file
         imageio.mimsave(video_file_name,image_frames,duration=frame_duration_s)
 
 
